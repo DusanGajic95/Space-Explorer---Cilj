34f42bbaf5b724ce6ce0a2ac80916b5c501bf71f
DusanGajic95 <159827270+DusanGajic95@users.noreply.github.com>
2024-02-26 23:04:17 +0100

    a

--- script.js
+++ script.js
@@ -18,7 +18,8 @@
     } catch (error) {
         console.error("Error fetching rocket details:", error);
     }
- }
+ } 
+ 
   
   function navigateTo(section) {
     const sections = [ "launches", "rockets", "company"];
@@ -50,19 +51,33 @@
   
   async function displayAboutUs() {
     const aboutSection = document.getElementById("company");
-    aboutSection.innerHTML = ""; 
+    if(aboutSection){
+
+      aboutSection.innerHTML = ""; 
+      
+      
+      const aboutEndpoint = "https://api.spacexdata.com/v4/company";
+      const aboutData = await fetchData(aboutEndpoint);
+      
+      
+      
+        const aboutInfo = document.createElement("div")
+        aboutInfo.innerHTML = `
+        <h2>${await aboutData.name}</h2>
+        <p>Osnivac: ${ await aboutData.founder}</p>
+        <p>Sediste: ${ await aboutData.headquarters.address}</p>
+        <p>Grad: ${ await aboutData.headquarters.city}</p>
+        <p>Drzava: ${ await aboutData.headquarters.state}</p> 
+        <p>Godina osnivanja: ${await aboutData.founded}</p>
+        <p>Opis: ${await aboutData.summary}</p>
+
+        `
+        aboutSection.appendChild(aboutInfo)
+        
+        
+      
+    }
     
-    
-    const aboutEndpoint = "https://api.spacexdata.com/v4/company";
-    const aboutData = await fetchData(aboutEndpoint);
-  
-    
-    aboutSection.innerHTML = `
-        <h2>About Us</h2>
-        <p>${aboutData.summary}</p>
-    `;
-  
-    aboutSection.style.display = "block";
   }
   
   async function displayHome() {
@@ -73,11 +88,7 @@
     rocketImagesContainer.innerHTML = ""; 
   
     
-    const rocketImageURLs = [
-        "img1.jpg"
-        
-        
-    ];
+    
   
     rocketImageURLs.forEach(url => {
         const rocketImage = document.createElement("img");
@@ -105,7 +116,7 @@
             <p>Mission: ${launch.mission_name}</p>
             <p>Success: ${launch.success ? 'Yes' : 'No'}</p>
             <img src="${launch.links.patch.small || 'https://via.placeholder.com/150'}" alt="Rocket Image">
-            <a href="detail.html">Details</a>
+            <a href="#" onclick="displayRocketDetails('${launch.rocket}')">Details</a>
             `;
             launchesSection.appendChild(launchInfo);
         });
@@ -141,16 +152,55 @@
   async function displayRocketDetails() {
 
     const itemId = new URLSearchParams(window.location.search).get('id');
-    console.log(itemId)
+    
     const rocketData = await fetchRocketDetails(itemId);
     
-    console.log(rocketData)
+    
     document.getElementById('title').innerText = rocketData.name
     document.getElementById('tip').innerText = rocketData.type
     document.getElementById('height').innerText = rocketData.height.meters + 'm'
     document.getElementById('mass').innerText = rocketData.mass.kg + 'kg'
     document.getElementById('description').innerText = rocketData.description
-    document.getElementById('img').src = rocketData.flickr_images[0]
+    
+    getSlider(rocketData.flickr_images)
 
   }
+   function getSlider (photoList) {
+    const sliderElement = document.getElementById("slider");
+    
+    photoList.forEach((photoPath) => {
+        const imgElement = document.createElement("img");
+        imgElement.src = photoPath;
+        sliderElement.appendChild(imgElement);
+        imgElement.setAttribute("class", "sliderImg")
+    });
+
+    const slides = document.getElementsByClassName("sliderImg");
+    let currentSlideIndex = 0;
+    
+
+    function showSlide(index) {
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.outline = "";
+        }
+        slides[index].style.outline = "1px black solid";
+        document.getElementById('img').src = photoList[index] 
+    }
+
+    showSlide(currentSlideIndex);
+
+    document.getElementById("sledecaSlika").addEventListener("click", function () {
+        currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+        showSlide(currentSlideIndex);
+    });
+
+    document.getElementById("prethodnaSlika").addEventListener("click", function () {
+        currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+        showSlide(currentSlideIndex);
+    });
+    
+}
+
+
+
  
